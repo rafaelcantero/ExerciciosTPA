fe49cec8c78ba2cdbfb36b8acddc14a34b8e3c0a
rafaelcantero <rafaelhcantero@yahoo.com>
2025-11-14 14:16:12 -0300

    Create Ex2.java

---
+++ vetor/Ex2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+ 
+public class TabuadaVetor {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+ 
+        int[] A = new int[5];
+ 
+        System.out.println("Digite 5 números inteiros:");
+        for (int i = 0; i < 5; i++) {
+            A[i] = ler.nextInt();
+        }
+ 
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Tabuada de " + A[i] + ":");
+            for (int j = 1; j <= 10; j++) {
+                System.out.println(A[i] + "x" + j + "=" + (A[i] * j));
+            }
+        }
+ 
+        ler.close();
+    }
+}
